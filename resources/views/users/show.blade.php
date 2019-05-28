36bac250d67d2d3beb0f633e3d9cd7bfa87f4a4c
Wudi <973322300@qq.com>
2019-05-28 11:59:09 +0800

    编辑个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -17,18 +17,18 @@
             </div>
         </div>
      </div>
-     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-     <div class="card ">
-     <div class="card-body">
-     <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1
-     </div>
-     </div>
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+         <div class="card ">
+             <div class="card-body">
+                <h1 class="mb-0" style="font-size:22px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
+             </div>
+         </div>
      <hr>
      {{-- 用户发布的内容 --}}
      <div class="card ">
-     <div class="card-body">
-     暂无数据 ~_~
-     </div>
+         <div class="card-body">
+         暂无数据 ~_~
+         </div>
      </div>
      </div>
 </div>
