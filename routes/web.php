943b95c1480e8e568e3eba089f8a87c5773f6589
Wudi <973322300@qq.com>
2019-05-28 11:02:11 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Auth::routes(['verify' => true]);
 // Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 // Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
 // Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
