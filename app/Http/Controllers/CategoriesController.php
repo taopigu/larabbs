148853a24bae9228a7a32737d11b8ed966262467
Wudi <973322300@qq.com>
2019-06-11 15:00:29 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,8 +8,8 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category){
-          $topics = Topic::where('category_id', $category->id)->paginate(20);
+    public function show(Category $category, Request $request, Topic $topic){
+          $topics = Topic::withOrder($request->order)->where('category_id', $category->id)->paginate(20);
           return view('topics.index', compact('topics', 'category'));
     }
 }
--- app/models/Topic.php
+++ app/models/Topic.php
@@ -13,4 +13,25 @@ class Topic extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query, $order)
+    {
+        switch($order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReplied();
+                break;
+        }
+        return $query->with('user', 'category');
+    }
+
+    public function scopeRecentReplied($query){
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    public function scopeRecent($query){
+        return $query->orderBy('created_at', 'desc');
+    }
 }
